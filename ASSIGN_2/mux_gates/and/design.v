64163f053abf17d8c203b530b83558726717986d
Aman Gupta <108945367+silentaf@users.noreply.github.com>
2025-07-26 00:35:45 +0530

    Create design.v

---
+++ ASSIGN_2/mux_gates/and/design.v
@@ -0,0 +1,11 @@
+module and_mux(input[1:0] in, input sel, output reg out);
+  always @(*)
+    begin
+      case(sel)
+        1'b0: out=in[0];
+        1'b1: out=in[1];
+      endcase
+    end
+     
+
+endmodule
