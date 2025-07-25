532c3d0a52a65dd47c22d8548e4978404a76cd90
Aman Gupta <108945367+silentaf@users.noreply.github.com>
2025-07-25 23:31:32 +0530

    Create design.v

---
+++ ASSIGN_2/grey/design.v
@@ -0,0 +1,9 @@
+module grey(input[3:0] in, output[3:0] out);
+  assign out[0]= in[0] ^ in[1];
+  assign out[1] = in[1] ^ in[2];
+  assign out[2] = in[2] ^in[3];
+  assign out[3]=in[3];
+  
+  
+
+endmodule
