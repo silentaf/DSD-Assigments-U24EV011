cb44b89708d36dd030eee91b3e4a2e00f99f4616
Aman Gupta <108945367+silentaf@users.noreply.github.com>
2025-07-25 16:51:39 +0530

    Create design_dec_2*4.v

---
+++ ASSIGN_2/decoder 2*4 3*8/design_dec_2*4.v
@@ -0,0 +1,18 @@
+module decoder(input[1:0] in, input en, output reg[3:0] out);
+  always @(*)
+    begin
+      if(en)
+        begin
+
+          case(in)
+            2'b00: out=4'b0001;
+            2'b01: out=4'b0010;
+            2'b10: out=4'b0100;
+            2'b11: out=4'b1000;
+          endcase
+        end
+      else
+        out=4'b0000;
+    end
+
+endmodule
