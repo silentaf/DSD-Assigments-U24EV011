4b490d8de53b2bf5a3208caa97e39b0923cf37f9
Aman Gupta <108945367+silentaf@users.noreply.github.com>
2025-07-25 15:05:05 +0530

    Delete ASSIGN_1/1bit_FA_structural.v

--- ASSIGN_1/1bit_FA_structural.v
+++
@@ -1,9 +0,0 @@
-module fa_stru(input a, input b, input cin, output cout, output sum);
-    wire t1, t2, t3;
-    
-    xor(t1, a, b);
-    xor(sum, t1, cin);
-    and(t2, a, b);
-    and(t3, t1, cin);
-    or(cout, t3, t2);
-endmodule
